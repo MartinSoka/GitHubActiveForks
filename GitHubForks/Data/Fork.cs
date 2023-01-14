71ec04acf847c4f754ee7af537359662cbbf9a5f
Martin Soka <martin.soka@tapkey.com>
2023-01-14 14:01:49 +0100

    Initial commit

---
+++ GitHubForks/Data/Fork.cs
@@ -0,0 +1,19 @@
+﻿namespace GitHubForks.Data
+{
+    public class Fork
+    {
+        public string url { get; set; }
+        public long id { get; set; }
+        public Owner owner { get; set; }
+        public string name { get; set; }
+        public DateTime pushed_at { get; set; }
+        public int stargazers_count { get; set; }
+        public int open_issues_count { get; set; }
+    }
+
+    public class Owner
+    {
+        public string avatar_url { get; set; }
+        public int id { get; set; }
+    }
+}
---
+++ GitHubForks/Data/GitHubService.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+
+namespace GitHubForks.Data
+{
+    public class GitHubService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public GitHubService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<List<Fork>> GetForksAsync(string url)
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("gitHubForks", "1.0"));
+            var response = await client.GetAsync(url).ConfigureAwait(false);
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new ApplicationException($"The response from the server was not successfull, reason: {response.ReasonPhrase}");
+            }
+            var json = await response.Content.ReadAsStringAsync();
+            var forks = JsonConvert.DeserializeObject<List<Fork>>(json);
+
+            return forks;
+
+        }
+    }
+}
+
